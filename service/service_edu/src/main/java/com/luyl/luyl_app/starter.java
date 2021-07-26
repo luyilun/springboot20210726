4a93ebfc8f5b583c24407f9885cd7c7b71109e33
luyilun <936894476@qq.com>
2021-07-26 21:56:56 +0800

    【修改】 更新

---
+++ service/service_edu/src/main/java/com/luyl/luyl_app/starter.java
@@ -0,0 +1,10 @@
+package com.luyl.luyl_app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class starter {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
---
+++ service/service_edu/src/test/java/com/luyl/luyl_app/testStarter.java
@@ -0,0 +1,8 @@
+package com.luyl.luyl_app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class testStarter {
+
+}
